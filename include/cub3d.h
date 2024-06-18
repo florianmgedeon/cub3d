3d2f495cf41b125264251fc5dbba5d390b0997fe
jkoupy <josef.koupy@seznam.cz>
2024-06-18 15:28:00 +0200

    more structs

--- include/cub3d.h
+++ include/cub3d.h
@@ -6,12 +6,22 @@
 /*   By: jkoupy <jkoupy@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/18 15:01:06 by jkoupy            #+#    #+#             */
-/*   Updated: 2024/06/18 15:07:31 by jkoupy           ###   ########.fr       */
+/*   Updated: 2024/06/18 15:27:21 by jkoupy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CUB3D_H
 # define CUB3D_H
+
+//keys
+# define LEFT 123
+# define RIGHT 124
+# define W 13
+# define A 0
+# define S 1
+# define D 2
+# define ESC 53
+
 
 # include "../library/libft/include/libft.h"
 # include "../library/get_next_line/include/get_next_line.h"
@@ -34,12 +44,23 @@ typedef struct s_color
 
 typedef struct s_map
 {
-	char		*name;
-	char		*path;
+	char 		*no_path;
+	char 		*so_path;
+	char 		*we_path;
+	char 		*ea_path;
+	t_color		floor;
+	t_color		ceiling;
 	int			size_x;
 	int			size_y;
 	int			**data;
 }	t_map;
+
+typedef struct s_player
+{
+	double	x;
+	double	y;
+	double	view;
+}	t_player;
 
 typedef struct s_data
 {
